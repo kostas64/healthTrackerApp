a2169b148769ff18402b8d9563870409847f0552
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-10-26 14:06:57 +0300

    discard modal

---
+++ src/components/DiscardModal.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+import Button from './Button';
+import {DimUtils} from '../utils/DimensionUtils';
+import {WIDTH, colors} from '../assets/constants';
+
+const DiscardModal = ({onPressDone, cancelModal}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.question}>
+        Are you sure you'd like to discard this activity?
+      </Text>
+      <View style={styles.hr} />
+
+      <View style={styles.row}>
+        <Button
+          label={'Cancel'}
+          onPress={cancelModal}
+          buttonContainerStyle={styles.buttonContainer}
+        />
+        <View style={styles.space} />
+        <Button
+          secondary
+          label={'Yes'}
+          onPress={onPressDone}
+          buttonContainerStyle={styles.buttonContainer}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  question: {
+    textAlign: 'center',
+    fontFamily: 'Rubik-Regular',
+    fontSize: DimUtils.getFontSize(14),
+  },
+  buttonContainer: {
+    width: (WIDTH - DimUtils.getDP(104)) / 2,
+  },
+  hr: {
+    marginVertical: DimUtils.getDP(16),
+    width: WIDTH - DimUtils.getDP(48),
+    height: 1,
+    backgroundColor: colors.lightGrey,
+  },
+  sapce: {
+    width: DimUtils.getDP(16),
+  },
+});
+
+export default DiscardModal;
