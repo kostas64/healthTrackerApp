a3e613270f8114b1899aad348834d4140805b358
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-10-29 21:43:39 +0200

    hide splash app

--- App.jsx
+++ App.jsx
@@ -1,21 +1,18 @@
 import React, {useEffect} from 'react';
-import BootSplash from 'react-native-bootsplash';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import {Navigation} from './src/router/BottomTab';
 import ContextProvider from './src/context/Context';
+import {hideSplash} from './src/utils/GenericUtils';
 import StatusBarManager from './src/components/StatusBarManager';
 
 const App = () => {
   useEffect(() => {
-    const hideSplash = async () => {
-      await BootSplash.hide({fade: true});
-    };
-
     hideSplash();
   }, []);
+
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <SafeAreaProvider style={{flex: 1}}>
--- src/utils/GenericUtils.js
+++ src/utils/GenericUtils.js
@@ -1,4 +1,10 @@
+import BootSplash from 'react-native-bootsplash';
+
 import {caloriesBurned} from '../assets/calories';
 
 export const getCaloriesBurned = (activity, value) =>
   value * caloriesBurned[activity];
+
+export const hideSplash = async () => {
+  await BootSplash.hide({fade: true});
+};
