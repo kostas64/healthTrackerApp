98eaf2a053b9656993178492ae88456f17ad9a04
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-10-29 21:42:01 +0200

    assets for bootsplash, bootsplash lib + init, hide it in app

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import BootSplash from 'react-native-bootsplash';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
@@ -8,6 +9,13 @@ import ContextProvider from './src/context/Context';
 import StatusBarManager from './src/components/StatusBarManager';
 
 const App = () => {
+  useEffect(() => {
+    const hideSplash = async () => {
+      await BootSplash.hide({fade: true});
+    };
+
+    hideSplash();
+  }, []);
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <SafeAreaProvider style={{flex: 1}}>
--- android/app/src/main/java/com/healthtrackerapp/MainActivity.java
+++ android/app/src/main/java/com/healthtrackerapp/MainActivity.java
@@ -6,9 +6,13 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+//Splash screen imports
+import com.zoontek.rnbootsplash.RNBootSplash;
+
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this, R.style.BootTheme);
     super.onCreate(null);
   }
 
